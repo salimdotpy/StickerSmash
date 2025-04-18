572df23f93ad3bc12a82b06ae35291c3020a3517
salimdotpy <salimdotpy@gmail.com>
2025-04-18 11:40:57 +0100

    made change in index

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href={'/about'} style={styles.button}>
+        Goto About screen
+      </Link>
     </View>
   );
 }
@@ -16,6 +20,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
     color: '#fff',
   }
 })
