b6c4e048ab730b5c9dd311030825f4335bd45eb3
salimdotpy <salimdotpy@gmail.com>
2025-04-18 12:08:43 +0100

    new update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,8 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen name="index" options={{ title: 'Home'}} />
       <Stack.Screen name="about" options={{ title: 'About'}}/>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
