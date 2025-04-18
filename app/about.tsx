e5d04105463a544dc5436ef8e127b4214c1443d8
salimdotpy <salimdotpy@gmail.com>
2025-04-18 11:31:52 +0100

    adding about screen

---
+++ app/about.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AboutScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>About Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#fff',
+    },
+});
