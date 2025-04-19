26e3ca4dadedf35bcd8b4013df8867edcc6ff8fc
salimdotpy <salimdotpy@gmail.com>
2025-04-19 15:41:14 +0100

    create emoji sticker component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,12 @@ import ImageViewer from "../components/ImageViewer";
 import Button from "../components/Button";
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from "react";
+import { type ImageSource } from 'expo-image';
+
 import IconButton from "../components/IconButton";
 import CircleButton from "../components/CircleButton";
 import EmojiPicker from "../components/EmojiPicker";
+import EmojiList from '../components/EmojiList';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -13,6 +16,7 @@ export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
   
   const pickImageAsync =async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -64,7 +68,7 @@ export default function Index() {
       </View>
       )}
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        {/* A list of emoji component will go here */}
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
     </View>
   );
