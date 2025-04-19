56987ab461483cc14c4c35948dc4faad77473800
salimdotpy <salimdotpy@gmail.com>
2025-04-19 15:28:04 +0100

    using icon & circle btns

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,8 @@ import ImageViewer from "../components/ImageViewer";
 import Button from "../components/Button";
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from "react";
+import IconButton from "../components/IconButton";
+import CircleButton from "../components/CircleButton";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -23,13 +25,31 @@ export default function Index() {
       alert('You did not select any image.');
     }
   }
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // we will implement this later
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
       <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       { showAppOptions ? (
-        <View />
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
       ) :
       (
       <View style={styles.footerContainer}>
@@ -54,5 +74,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 })
