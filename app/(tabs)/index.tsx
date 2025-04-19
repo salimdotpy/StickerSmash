702eaa9fab3ad3721a1f0a5763f7b79433279866
salimdotpy <salimdotpy@gmail.com>
2025-04-19 15:10:12 +0100

    new update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,13 @@ import { View, StyleSheet } from "react-native";
 import ImageViewer from "../components/ImageViewer";
 import Button from "../components/Button";
 import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  
   const pickImageAsync =async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
