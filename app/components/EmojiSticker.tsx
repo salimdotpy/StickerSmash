8cd0b523d743bf287359d20c655bfc86a7b7056b
salimdotpy <salimdotpy@gmail.com>
2025-04-21 10:33:25 +0100

    Update the EmojiSticker component

--- app/components/EmojiSticker.tsx
+++ app/components/EmojiSticker.tsx
@@ -30,15 +30,35 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     };
   });
 
+  const drag = Gesture.Pan().onChange(event => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+  
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <Animated.View style={{ top: -350 }}>
-       <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </GestureDetector>
-    </Animated.View>
+    <GestureDetector gesture={drag}>
+        <Animated.View style={[containerStyle, { top: -350 }]}>
+            <GestureDetector gesture={doubleTap}>
+                <Animated.Image
+                source={stickerSource}
+                resizeMode="contain"
+                style={[imageStyle, { width: imageSize, height: imageSize }]}
+                />
+            </GestureDetector>
+        </Animated.View>
+    </GestureDetector>
   );
 }
